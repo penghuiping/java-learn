ed1fef1ca011995b324625b5276fa5d64d59cd03
penghuiping <575813261@qq.com>
2019-06-28 16:00:00 +0800

    String

--- src/test/java/basic/ArrayTest.java
+++ src/test/java/basic/ArrayTest.java
@@ -5,12 +5,34 @@ import org.junit.Test;
 /**
  * @author: penghuiping
  * @date: 2019/6/27 10:19
- * @description:
+ * @description: 数组相关操作
  */
 public class ArrayTest {
 
     @Test
     public void test() {
+        var arr = new int[]{1, 2, 3, 4, 5, 6, 7};
+        for (var i = 0; i < arr.length; i++) {
+            System.out.print(i);
+        }
+        System.out.println();
 
+        //取值
+        int a = arr[0];
+        System.out.println(a);
+
+
+        //二维数组
+        var arr1 = new int[3][3];
+        arr1[0] = new int[]{1, 2, 3};
+        arr1[1] = new int[]{3, 1, 2};
+        arr1[2] = new int[]{11, 2, 34};
+
+        for(int i=0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+                System.out.print(arr1[i][j]+" ");
+            }
+            System.out.println();
+        }
     }
 }
--- src/test/java/basic/StringTest.java
+++ src/test/java/basic/StringTest.java
@@ -1,9 +1,16 @@
 package basic;
 
+import org.junit.Test;
+
 /**
  * @author: penghuiping
  * @date: 2019/6/28 15:46
- * @description:
+ * @description: 字符串相关操作
  */
 public class StringTest {
+
+    @Test
+    public void test() {
+
+    }
 }
